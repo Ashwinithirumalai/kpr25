9944ac87fd32724d5a8fc3fb440bc6823c36f24d
Ashwinithirumalai <ashusathiya556@gmail.com>
2025-06-09 16:29:21 +0530

    REGISTRATION.HTML

---
+++ assets/javascript.js/python/demo.py
@@ -0,0 +1,16 @@
+"""def add():
+    print(10+20)"""
+"""list=[1,2,3,4,5]
+for i in list:
+    print(i)"""
+"""tuple=(6,7,8,9,10);                                                                                                                                                                                      
+for i in tuple:
+    print(i)
+    print(type(i))
+    print(type(tuple))"""
+
+dictionary= {"name":"ashu06","password":"12345678"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary[key])
